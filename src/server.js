1f25a5f541f589b7feab242934c1963172416bdd
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-09-12 21:54:34 -0600

    WIP deploy to production

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const app = require("./app");
+const http = require("http");
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+server.on("listening", () => {
+  console.log(`server is listening for requests on port ${server.address().port}`);
+ });
